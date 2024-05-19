c18be6e335fd2fcc3e190618548db845f1f025f2
Davydovich <kostiarabota@gmail.com>
2024-05-19 22:15:24 +0300

    feat/SCRUM-57_65

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,1 +1,41 @@
+import RouterService from '../../services/router/router.service';
+import BaseComponent from '../base/base.component';
+import Button from '../button/button.component';
 import './main.scss';
+
+export default class MainComponent extends BaseComponent<'div'> {
+  constructor(private router: RouterService) {
+    super({ tag: 'div', classes: ['main'] });
+    this.createButtons();
+  }
+
+  private createButtons() {
+    const loginButton = new Button({
+      text: 'Log In',
+      onClick: () => {
+        this.router.navigate('/login');
+      },
+    });
+    const registrationButton = new Button({
+      text: 'Registration',
+      onClick: () => {
+        this.router.navigate('/registration');
+      },
+    });
+    const notFound = new Button({
+      text: '404',
+      onClick: () => {
+        this.router.navigate('/404');
+      },
+    });
+
+    const main = new Button({
+      text: 'Main',
+      onClick: () => {
+        this.router.navigate('/');
+      },
+    });
+
+    this.append([main, loginButton, registrationButton, notFound]);
+  }
+}
