56352db22cb1d6c22388574dac2b56a1e6766099
Herman <hermanvasilkouskiy@gmail.com>
2024-05-29 00:31:49 +0300

    feat/SCRUM-114

---
+++ src/app/models/product/product-price.model.ts
@@ -0,0 +1,5 @@
+export default interface ProductPrice {
+  id: string;
+  centAmount: number;
+  currencyCode: string;
+}
--- src/app/models/product/product.model.ts
+++ src/app/models/product/product.model.ts
@@ -1,7 +1,9 @@
 import ProductImageDTO from './product-DTO/product-image-DTO.model';
+import ProductPrice from './product-price.model';
 
 export default interface Product {
   name: string;
   description: string;
   images: ProductImageDTO[];
+  prices: ProductPrice[];
 }
