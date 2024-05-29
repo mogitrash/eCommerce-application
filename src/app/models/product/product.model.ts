08b90f297c4e6b399b837683df5a9d64b11b0efc
Herman Vasilkouskiy <140188066+mogitrash@users.noreply.github.com>
2024-05-29 23:22:40 +0300

    feat/SCRUM-114 (#30)
    * feat/SCRUM-114
    
    * refactor: compress product models to single file

--- src/app/models/product/product.model.ts
+++ src/app/models/product/product.model.ts
@@ -2,9 +2,16 @@ export interface Product {
   name: string;
   description: string;
   images: ProductImage[];
+  prices: ProductPrice[];
 }
 
 export interface ProductImage {
   url: string;
   label: string;
+}
+
+export interface ProductPrice {
+  id: string;
+  centAmount: number;
+  currencyCode: string;
 }
--- src/app/utilities/product-DTO-converter.ts
+++ src/app/utilities/product-DTO-converter.ts
@@ -2,10 +2,15 @@ import { ProductDTO } from '../models/product/product-DTO.model';
 import { Product } from '../models/product/product.model';
 import productImageDTOConverter from './product-image-DTO-converter';
 
-const productDTOConverter = (productDTO: ProductDTO): Product => ({
-  name: productDTO.name['en-US'],
-  description: productDTO.description['en-US'],
-  images: productDTO.masterVariant.images.map(productImageDTOConverter),
+const productDTOConverter = ({ name, description, masterVariant }: ProductDTO): Product => ({
+  name: name['en-US'],
+  description: description['en-US'],
+  images: masterVariant.images.map(productImageDTOConverter),
+  prices: masterVariant.prices.map((priceDTO) => ({
+    id: priceDTO.id,
+    centAmount: priceDTO.value.centAmount,
+    currencyCode: priceDTO.value.currencyCode,
+  })),
 });
 
 export default productDTOConverter;
