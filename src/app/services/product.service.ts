098fce43dcd38667f9384e75ea58729def5b19ec
Herman <hermanvasilkouskiy@gmail.com>
2024-05-31 12:56:50 +0300

    feat: implement category service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,10 +1,9 @@
-import LocalStorageEndpoint from '../models/local-storage-endpoint.model';
 import { GetAllPublishedProductsResponseDTO } from '../models/product/product-DTO.model';
 import { GetAllPublishedProductsRequest, Product } from '../models/product/product.model';
 import getAllPublishedProductsRequestConverter from '../utilities/get-all-published-products-request-converter';
+import getCurrentAccessToken from '../utilities/get-current-access-token';
 import productDTOConverter from '../utilities/product-DTO-converter';
 import authorizationService from './authorization.service';
-
 
 export default class ProductService {
   private projectKey = process.env.CTP_PROJECT_KEY;
@@ -16,15 +15,7 @@ export default class ProductService {
   async getAllPublishedProducts(
     parameters: GetAllPublishedProductsRequest = {},
   ): Promise<GetAllPublishedProductsResponseDTO> {
-    let token = localStorage.getItem(LocalStorageEndpoint.userToken);
-
-    // TODO: implement token refreshing
-    if (!token) {
-      const authorizationResponse = await this.authorizationService.getAnonymousSessionToken();
-      if ('access_token' in authorizationResponse) {
-        token = authorizationResponse.access_token;
-      }
-    }
+    const token = await getCurrentAccessToken();
 
     const queryParameters = new URLSearchParams(
       Object.entries(getAllPublishedProductsRequestConverter(parameters)),
@@ -44,14 +35,7 @@ export default class ProductService {
   }
 
   async getPublishedProductById(id: string): Promise<Product> {
-    let token = localStorage.getItem(LocalStorageEndpoint.userToken);
-
-    if (!token) {
-      const authorizationResponse = await this.authorizationService.getAnonymousSessionToken();
-      if ('access_token' in authorizationResponse) {
-        token = authorizationResponse.access_token;
-      }
-    }
+    const token = await getCurrentAccessToken();
 
     const headers = new Headers({
       Authorization: `Bearer ${token}`,
@@ -63,6 +47,5 @@ export default class ProductService {
     })
       .then((res) => res.json())
       .then(productDTOConverter);
-
   }
 }
