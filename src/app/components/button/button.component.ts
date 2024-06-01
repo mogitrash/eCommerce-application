285d8498b8a2a741483a7f8db60926a49e9f796c
Aliaksandra Rahachova <111020592+rahachova@users.noreply.github.com>
2024-06-01 23:00:26 +0200

    feat/SCRUM-98 (#43)
    * feat: add address management
    
    * fix: change button style names

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,15 +1,26 @@
 import './button.scss';
 import BaseComponent from '../base/base.component';
 
-type ButtonConfig = { text: string; onClick?: (event: Event) => void };
+type ButtonSize = 'small';
+
+type ButtonStyle = 'positive' | 'negative';
+
+type ButtonType = 'button' | 'submit';
+
+type ButtonConfig = {
+  text: string;
+  onClick?: (event: Event) => void;
+  disabled?: boolean;
+  size?: ButtonSize;
+  style?: ButtonStyle;
+  type?: ButtonType;
+};
 
 export default class Button extends BaseComponent<'button'> {
   constructor(config: ButtonConfig) {
-    const { text, onClick } = config;
+    const { text } = config;
     super({ tag: 'button', classes: ['button'], textContent: text });
-    if (onClick) {
-      this.addListener('click', onClick);
-    }
+    this.setupButton(config);
   }
 
   public disable() {
@@ -18,5 +29,24 @@ export default class Button extends BaseComponent<'button'> {
 
   public enable() {
     this.getElement().removeAttribute('disabled');
+  }
+
+  private setupButton(config: ButtonConfig): void {
+    const { onClick, disabled, size, style, type } = config;
+    if (onClick) {
+      this.addListener('click', onClick);
+    }
+    if (disabled) {
+      this.disable();
+    }
+    if (size) {
+      this.addClass(`button--${size}`);
+    }
+    if (style) {
+      this.addClass(`button--${style}`);
+    }
+    if (type) {
+      this.setAttribute('type', type);
+    }
   }
 }
